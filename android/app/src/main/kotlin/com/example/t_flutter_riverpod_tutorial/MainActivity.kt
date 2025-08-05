d52451385e959390454ced482ef630f780e31659
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-08-05 19:25:56 +0530

    after flutter upgrade

--- android/app/src/main/kotlin/com/example/t_flutter_riverpod_tutorial/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/t_flutter_riverpod_tutorial/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.t_flutter_riverpod_tutorial
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
